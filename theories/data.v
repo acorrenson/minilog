fe84058c2f903d0453de5d7e085398e56967d804
Arthur Correnson <arthur.correnson@gmail.com>
2022-11-15 23:02:41 +0100

    SImplify the syntax to avoid redundancy : constants are defined as functors applicated to nil

--- theories/data.v
+++ theories/data.v
@@ -6,22 +6,30 @@ Import ListNotations.
 
 (** ** Syntax *)
 
-(** A [datum] is either a constant or a function symbol applied to a set of [datum] *)
+(** A [datum] is function symbol applied to a set of [datum] *)
 Inductive datum :=
-  | Dcst : string -> datum
   | Dcmp : string -> list datum -> datum.
+
+Definition Dcst (c : string) := Dcmp c [].
 
 (** Patterns over [datum] *)
 Inductive pattern :=
   | Pvar : nat -> pattern
-  | Pcst : string -> pattern
   | Pcmp : string -> list pattern -> pattern.
+
+Definition Pcst (c : string) := Pcmp c [].
 
 (** Free variables of a pattern *)
 Inductive free_var : pattern -> nat -> Prop :=
   | free_var_var x : free_var (Pvar x) x
   | free_var_cmp x f l :
     Exists (fun p => free_var p x) l -> free_var (Pcmp f l) x.
+
+Definition is_free (p : pattern) (x : nat) : bool :=
+  true (* todo *).
+
+Axiom is_free_iff:
+  forall p x, is_free p x = true <-> free_var p x.
 
 (** ** Induction Principles *)
 
@@ -30,17 +38,15 @@ Inductive free_var : pattern -> nat -> Prop :=
 *)
 Fixpoint datum_induction
   (P : datum -> Prop)
-  (H0 : forall c, P (Dcst c))
   (Hind : forall f dats, Forall P dats -> P (Dcmp f dats))
   (dat : datum) : P dat :=
   match dat with
-  | Dcst n => H0 n
   | Dcmp f dats =>
     let helper := fix F l :=
       match l as l' return Forall P l' with
       | nil => Forall_nil P
       | cons x xs =>
-        @Forall_cons _ P x xs (datum_induction P H0 Hind x) (F xs)
+        @Forall_cons _ P x xs (datum_induction P Hind x) (F xs)
       end
     in
     Hind f dats (helper dats)
@@ -51,19 +57,17 @@ Fixpoint datum_induction
 *)
 Fixpoint pattern_induction
   (P : pattern -> Prop)
-  (H0 : forall c, P (Pcst c))
   (H1 : forall x, P (Pvar x))
   (Hind : forall f dats, Forall P dats -> P (Pcmp f dats))
   (pat : pattern) : P pat :=
   match pat with
-  | Pcst n => H0 n
   | Pvar x => H1 x
   | Pcmp f pats =>
     let helper := fix F l :=
       match l as l' return Forall P l' with
       | nil => Forall_nil P
       | cons x xs =>
-        @Forall_cons _ P x xs (pattern_induction P H0 H1 Hind x) (F xs)
+        @Forall_cons _ P x xs (pattern_induction P H1 Hind x) (F xs)
       end
     in
     Hind f pats (helper pats)
@@ -76,14 +80,12 @@ Delimit Scope pattern with pattern.
 
 Fixpoint datum_size (d : datum) : nat :=
   match d with
-  | Dcst _ => 1
   | Dcmp _ dats =>
     1 + sum datum_size dats
   end.
 
 Fixpoint pattern_size (p : pattern) : nat :=
   match p with
-  | Pcst _ => 1
   | Pvar _ => 1
   | Pcmp _ pats =>
     1 + sum pattern_size pats
@@ -92,7 +94,6 @@ Fixpoint pattern_size (p : pattern) : nat :=
 (** Boolean equality for [datum] *)
 Fixpoint datum_eqb (d1 d2 : datum) : bool :=
   match d1, d2 with
-  | Dcst c1, Dcst c2 => (c1 =? c2)%string
   | Dcmp f1 l1, Dcmp f2 l2 =>
     let fix F l1 l2 :=
       match l1, l2 with
@@ -101,43 +102,39 @@ Fixpoint datum_eqb (d1 d2 : datum) : bool :=
       | _, _ => false
       end
     in andb (f1 =? f2)%string (F l1 l2)
-  | _, _ => false
   end.
 
 Lemma datum_eqb_refl:
   forall x, datum_eqb x x = true.
 Proof.
-  refine (datum_induction _ _ _); intros; simpl.
-  + apply String.eqb_refl.
-  + apply Bool.andb_true_iff; split.
-    apply String.eqb_refl.
-    induction dats; try easy.
-    inversion H as [| ? ? H1 H2]; subst.
-    rewrite H1. simpl.
-    now apply IHdats.
+  refine (datum_induction _ _); intros; simpl.
+  apply Bool.andb_true_iff; split.
+  apply String.eqb_refl.
+  induction dats; try easy.
+  inversion H as [| ? ? H1 H2]; subst.
+  rewrite H1. simpl.
+  now apply IHdats.
 Qed.
 
 Lemma datum_eqb_eq:
   forall x y, datum_eqb x y = true <-> x = y.
 Proof.
-  refine (datum_induction _ _ _).
-  - intros c []; simpl; try easy.
-    now rewrite String.eqb_eq; split; intros [=->].
-  - intros f dats IH y. split; intros.
-    + destruct y; simpl in H; try easy.
-      apply Bool.andb_true_iff in H as [->%String.eqb_eq H].
-      induction l in dats, H, IH, s |-*.
-      destruct dats; try easy.
-      destruct dats; try easy.
-      apply Bool.andb_true_iff in H as [H1 H2].
-      inversion_clear IH as [|? ? H3 H4].
-      apply H3 in H1 as ->.
-      assert (Dcmp s dats = Dcmp s l).
-      apply IHl; auto.
-      now inversion H.
-    + destruct y; simpl in H; try easy.
-      inversion_clear H.
-      apply datum_eqb_refl.
+  refine (datum_induction _ _).
+  intros f dats IH y. split; intros.
+  + destruct y; simpl in H; try easy.
+    apply Bool.andb_true_iff in H as [->%String.eqb_eq H].
+    induction l in dats, H, IH, s |-*.
+    destruct dats; try easy.
+    destruct dats; try easy.
+    apply Bool.andb_true_iff in H as [H1 H2].
+    inversion_clear IH as [|? ? H3 H4].
+    apply H3 in H1 as ->.
+    assert (Dcmp s dats = Dcmp s l).
+    apply IHl; auto.
+    now inversion H.
+  + destruct y; simpl in H; try easy.
+    inversion_clear H.
+    apply datum_eqb_refl.
 Qed.
 
 Lemma datum_eqb_neq:
@@ -161,7 +158,6 @@ Definition psubstitution := nat -> pattern.
 (** Apply a closed substitution to a pattern *)
 Fixpoint csubst (p : pattern) (sub : csubstitution) : datum :=
   match p with
-  | Pcst c => Dcst c
   | Pcmp f pats =>
     Dcmp f (map (fun p => csubst p sub) pats)
   | Pvar n => sub n
@@ -170,7 +166,6 @@ Fixpoint csubst (p : pattern) (sub : csubstitution) : datum :=
 (** Apply an open substitution to a pattern *)
 Fixpoint psubst (p : pattern) (sub : psubstitution) : pattern :=
   match p with
-  | Pcst c => p
   | Pcmp f pats =>
     Pcmp f (map (fun p => psubst p sub) pats)
   | Pvar n => sub n
@@ -187,3 +182,29 @@ Definition Match (dat : datum) (pat : pattern) : Prop :=
 (** What is means that two patterns are unifiable *)
 Definition Unify (pat1 pat2 : pattern) : Prop :=
   exists sub, psubst pat1 sub = psubst pat2 sub.
+
+(** Composing two subtitutions ([s2] after [s1]) *)
+Definition compose (s2 s1 : psubstitution) (n : nat) : pattern :=
+  psubst (s1 n) s2.
+
+Infix "•" := compose (at level 80).
+
+Definition equ (s1 s2 : psubstitution) :=
+  forall n, s1 n = s2 n.
+
+Infix "≡" := equ (at level 90).
+
+Definition subst_le (s1 s2 : psubstitution) : Prop :=
+  exists s3, s3 • s1 ≡ s2.
+
+Infix "⪯" := subst_le (at level 90).
+
+Lemma psubst_compose :
+  forall s1 s2 t, psubst t (s2 • s1) = psubst (psubst t s1) s2.
+Proof.
+  intros s1 s2. apply pattern_induction; simpl; try easy.
+  intros f ts Hts. induction ts; simpl; auto.
+  f_equal. inversion Hts; subst. rewrite H1.
+  specialize (IHts H2). clear H2.
+  simpl in IHts. now injection IHts as ->.
+Qed.
